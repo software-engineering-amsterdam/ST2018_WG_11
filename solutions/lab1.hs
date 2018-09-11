1e3d4d0811e1626b8ccf7ea3f9aee5b7babaacf6
stephankok <stephankok33@gmail.com>
2018-09-11 13:38:48 +0200

    assignment 1

--- solutions/lab1.hs
+++ solutions/lab1.hs
@@ -25,3 +25,29 @@ data Boy = Matthew | Peter | Jack | Arnold | Carl
 
 boys = [Matthew, Peter, Jack, Arnold, Carl]
 
+-- 1. Redo exercises 2 and 3 of Workshop 1 by writing QuickCheck tests for these statements
+
+sumsquare :: Integer -> Integer
+sumsquare 0 = 0
+sumsquare n = n ^ 2 + sumsquare (n - 1)
+
+sumsquarefunc :: Integer -> Integer
+sumsquarefunc n = div (n * (n + 1)*(2*n + 1)) 6
+
+testsumsquare :: Integer -> Bool
+testsumsquare x = (x > 0) --> sumSquare x == func x
+
+
+-- 2. Redo exercise 4 of Workshop 1 by replacing sets by lists, and testing the property for integer lists of the form [1..n]
+
+-- 3. Redo exercise 5 of Workshop 1 by replacing sets by lists, and testing the property for integer lists of the form [1..n]
+
+-- 4. The natural number 13 has the property that it is prime and its reversal, the number 31, is also prime. Write a function that finds all primes < 10000 with this property.
+
+-- 5. Find the smallest prime number that is a sum of 101 consecutive primes
+
+-- 6. What is the smallest counterexample?
+
+-- 7. Implement and test the Luhn Algorithm
+
+-- 8. Crime Scene Investigation. Time: +/- 60 minutes first (wrong) solution, +/- 60 min new solution
