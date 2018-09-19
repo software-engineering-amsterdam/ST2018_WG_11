e256e7036c84e28590ffcdf34004839d13402af9
stephankok <stephankok33@gmail.com>
2018-09-19 16:09:17 +0200

    lab3 init

---
+++ stephan/Lab3.hs
@@ -0,0 +1,24 @@
+module Lab3 where
+import Data.List
+import System.Random
+import Test.QuickCheck
+import Lecture3
+
+-- 1.
+
+{-
+    contradiction :: Form -> Bool
+
+    tautology :: Form -> Bool
+
+    -- | logical entailment 
+    entails :: Form -> Form -> Bool
+
+    -- | logical equivalence
+    equiv :: Form -> Form -> Bool
+-}
+
+
+
+main = do
+    print "test"
