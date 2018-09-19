9f766d12edc5172ffef256c5520dcee8d0fafcf2
stephankok <stephankok33@gmail.com>
2018-09-19 16:41:03 +0200

    taut + con

--- stephan/Lab3.hs
+++ stephan/Lab3.hs
@@ -18,7 +18,23 @@ import Lecture3
     equiv :: Form -> Form -> Bool
 -}
 
+tautology :: Form -> Bool
+tautology f = all (\v -> evl v f) (allVals f)
+
+contradiction :: Form -> Bool
+contradiction f = all (\v -> evl v f == False) (allVals f)
+
+testTaut,testCon :: Form
+testTaut = Dsj [Prop 1, Neg (Prop 1)]
+testCon = Cnj [Prop 1, Neg (Prop 1)]
+
+assignment1 = do
+    print "Assignment 1"
+    print (tautology testTaut)
+    print (tautology testCon)
+    print (contradiction testCon)
+    print (contradiction testTaut)
 
 
 main = do
-    print "test"
+    assignment1
