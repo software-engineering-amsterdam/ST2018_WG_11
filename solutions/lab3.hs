3821253b9278164f6f7663612c2ed748a821005e
Pieter <pieterdonkers_3@hotmail.com>
2018-09-25 17:08:05 +0200

    added exercise 3

--- solutions/lab3.hs
+++ solutions/lab3.hs
@@ -49,7 +49,71 @@ assignment1 = do
 -- 2. Test the parse function for parsing propositional formulas.
 
 -- 3. Haskell program for converting formulas into CNF.
+strictCnf :: Form -> Form
+strictCnf (Prop x)                 = Prop x
+strictCnf (Neg x)                  = Neg (strictCnf x)
 
+--if only one argument is given this can be simplified
+strictCnf (Cnj (x:[])) = x
+strictCnf (Dsj (x:[])) = x
+
+-- Replace the (p v (q ^ r)) with ((p v q) ^ (p v r))
+strictCnf (Dsj [Cnj [x,y],z]) = Cnj [strictCnf(Dsj [z,x]),strictCnf(Dsj [z,y])]
+strictCnf (Dsj [z, Cnj[x,y]]) = Cnj [strictCnf(Dsj [z,x]),strictCnf(Dsj [z,y])]
+
+-- Check every element
+strictCnf (Dsj xs) = Dsj (map strictCnf xs)
+strictCnf (Cnj xs) = Cnj (map strictCnf xs)
+
+-- By enforcing that all the conjunction and disjunctions are in pair and not more we
+-- can ensure that the form p v (q ^ r) is always true
+-- If we dont this the functions crashed on inputs like Cnj [p,q,r]
+toPairs :: Form -> Form
+toPairs (Prop x) = Prop x
+toPairs (Neg x) = Neg (toPairs x)
+toPairs (Cnj (x:[])) = toPairs x
+toPairs (Cnj (x:xs)) | (length xs) > 1 = Cnj [toPairs x,toPairs(Cnj xs)]
+                     | otherwise = Cnj (map toPairs (x:xs))
+toPairs (Dsj (x:[])) = toPairs x
+toPairs (Dsj (x:xs)) | (length xs) > 1 = Dsj [toPairs x,toPairs(Dsj xs)]
+                     | otherwise = Dsj (map toPairs (x:xs))
+
+-- just reapplying untill it is in the correct format is key.
+-- if we do this we just look for the pattern
+-- when not doing this the algorithm isnt that good in circling back up
+-- so one you replace something in the 'bottom' the 'top' of the tree needs
+-- to be computed again
+cnf :: Form -> Form
+cnf frm = while (not . isCnf) strictCnf inp
+      where inp = toPairs (nnf (arrowfree frm))
+
+-- Check if the function is in cnf form.
+isCnf :: Form -> Bool
+isCnf (Prop x) = True
+isCnf (Neg (Prop x)) = True
+isCnf (Neg _) = False
+isCnf (Dsj xs) = not (any isCnj xs) && (all (==True) (map isCnf xs))
+isCnf (Cnj xs) = all (==True) (map isCnf xs)
+isCnf (Impl x y) = False
+isCnf (Equiv x y) = False
+
+-- Because appearantly we cant do list comprehension and do ==Cnj we add this fucntion.
+isCnj :: Form -> Bool
+isCnj (Cnj xs) = True
+isCnj _ = False
+
+assignment3 = do
+    print "exercise 3"
+    print "Check equivalence with original"
+    print (equiv (cnf form1) form1)
+    print (equiv (cnf form2) form2)
+    print (equiv (cnf form3) form3)
+    print (equiv (cnf td1) td1)
+    print (equiv (cnf td2) td2)
+    print "reapplying cnf on an already cnf should do nothing"
+    print (cnf form1 == (cnf (cnf form1)))
+    print (cnf form2 == (cnf (cnf form2)))
+    print (cnf form3 == (cnf (cnf form3)))
 -- 4. Generator.
 
 -- 5. Bonus.
