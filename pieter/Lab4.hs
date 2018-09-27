b69a407d43a8045fb8d24de0ac5e3ebb407dee1b
Pieter <pieterdonkers_3@hotmail.com>
2018-09-27 08:53:19 +0200

    added ex 4 and part of ex 5

--- pieter/Lab4.hs
+++ pieter/Lab4.hs
@@ -2,6 +2,7 @@ module Lab4 where
 
 import Data.List
 import Data.Char
+import Data.Tuple
 import Test.QuickCheck
 import SetOrd
 import System.Random
@@ -11,8 +12,8 @@ import System.Random
 -- Pretty much everything was clear but some exercises and 
 -- examples were vague like exercise 4.33 and 4.34
 
--- Exercise 4 Read Chapter 5 of The Haskell Road
-
+-- Exercise 2
+-- 60 minutes
 instance Arbitrary a => Arbitrary (Set a) where
     arbitrary = do
         xs <- arbitrary
@@ -39,10 +40,13 @@ doubleList (Set xs) = Set (map (*2) xs)
 testDouble :: Set Int -> Bool
 testDouble (Set xs) = (doubleList (Set xs)) == Set (map (*2) xs)
 
+-- This is already implemented in the provided SetOrd library
 -- unionSet :: (Ord a) => Set a -> Set a -> Set a 
 -- unionSet (Set [])     set2  =  set2
 -- unionSet (Set (x:xs)) set2  = 
 --    insertSet x (unionSet (Set xs) set2)
+
+-- Exercise 3 upto now like 30 minutes
 
 intersectionSet :: (Ord a) => Set a -> Set a -> Set a
 intersectionSet (Set []) _ = Set []
@@ -57,6 +61,29 @@ differenceSet (Set xs) (Set ys) = Set ([p | p <- xs, not( p `elem` ys)] ++ [q | 
 ts1 = Set [1,2,3,4,5]
 ts2 = Set [4,5,6,7]
 
+-- Exercise 4 Read Chapter 5 of The Haskell Road
+
+-- Exercise 5 20 minutes
+type Rel a = [(a,a)]
+
+-- returns the symetric closure of the relations
+-- Example: [(1,2),(2,3),(3,4)] -> [(1,2),(2,1),(2,3),(3,2),(3,4),(4,3)]
+symClos :: Ord a => Rel a -> Rel a
+symClos xs = xs ++ [(swap x) | x <- xs, not ((swap x) `elem` xs)]
+tr1 = [(1,2),(2,3),(3,4)]
+
+-- Exercise 6
+infixr 5 @@
+
+(@@) :: Eq a => Rel a -> Rel a -> Rel a
+r @@ s = nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
+
+tr2 = [(1,1)]
+-- returns the transitive closure of the relation
+-- Example: [(1,2),(2,3),(3,4)] -> [(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)]
+trClos :: Ord a => Rel a -> Rel a 
+trClos xs = nub (xs ++ (xs @@ xs))
+-- this should be repeated because new relations also need to be tested on transitivity
 
 main = do
     print "yoyoy"
