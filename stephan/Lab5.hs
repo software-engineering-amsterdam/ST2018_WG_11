4b3eac1baa218358ea4ba903fc2a232b3be80738
stephankok <stephankok33@gmail.com>
2018-10-04 11:50:31 +0200

    assignment1

---
+++ stephan/Lab5.hs
@@ -0,0 +1,16 @@
+
+module Lab5
+where 
+import Data.List
+import System.Random
+import Lecture5
+import Lab5_ex1 (assingment1)
+
+-- 1 See Lab5_ex1.hs
+
+-- 2
+
+
+main2 = do
+    print "Lab 5"
+    assingment1
