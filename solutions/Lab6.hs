a1b01a1ddb6eeb86c96f4662c83bf8175197ea34
stephankok <stephankok33@gmail.com>
2018-10-12 14:50:05 +0200

    commends

--- solutions/Lab6.hs
+++ solutions/Lab6.hs
@@ -12,8 +12,8 @@ import System.Random
 {-
     quickCheck function to test correctness
 -}
-mytest :: Integer -> Integer -> Integer -> Bool
-mytest b e m = e < 0 || m == 0 || (b^(e) `mod` m == (exM b (e) m))
+modulairCheck :: Integer -> Integer -> Integer -> Bool
+modulairCheck b e m = e < 0 || m == 0 || (b^(e) `mod` m == (exM b (e) m))
 
 {-
     Old method from lecture to compare running time
@@ -29,7 +29,7 @@ test2_fast = exM 9 99999999 13
 assignment1 = do
     print "---- Assignment 1 & 2"
     print "Testing runtime of different functions and correctness"
-    quickCheck (withMaxSuccess 10000 mytest)
+    quickCheck (withMaxSuccess 10000 modulairCheck)
     print ("To test which was faster I ran the above function in "
         ++ "the :set +s enviroment the results are printed below")
     print "test1 slow: exM' 9 9999999 13: (0.28 secs, 11,605,312 bytes)"
@@ -44,26 +44,29 @@ assignment1 = do
 
 --4 time: 30 min
 
--- returns first fail of prime that is not a prime
--- with k repetitions
+{-
+    Using little Fermat: 
+    return first fail of a identified prime that is not a prime
+
+    with k repetitions
+-}
 firstFail :: Int -> [Integer] -> IO Integer
 firstFail k (x:xs) = do
     isPrime <- primeTestsF k x
     next <- if isPrime then return x else firstFail k xs
     return $ next
 
--- Reversed list of first fail of found prime that is not a prime
+-- list of first fail of found prime that is not a prime
 fermatsCheck :: Int -> IO [Integer]
 fermatsCheck maxK = do
     first <- firstFail maxK composites
     next <- if (maxK > 1) then fermatsCheck (maxK - 1) else return []
-    return (first : next)
+    return (next ++ [first])
 
 assignment4 = do
     print "---- Assignment 4 - fermat"
     print "You see that as you increasing k first fail decreases really fast"
-    check <- fermatsCheck 9
-    print $ reverse $ check
+    fermatsCheck 9
 
 --5
 
@@ -81,6 +84,7 @@ assignment4 = do
     is not actually prime.
 -}
 
+-- Carmichael numbers
 carmichael :: [Integer]
 carmichael = [ (6*k+1)*(12*k+1)*(18*k+1) | 
     k <- [2..], 
@@ -88,6 +92,10 @@ carmichael = [ (6*k+1)*(12*k+1)*(18*k+1) |
     prime (12*k+1), 
     prime (18*k+1) ]
 
+{-
+    Using little Fermat check the first carmichael numbers
+    where the method falsely identifies a non-prime as prime
+-}
 carmichaelCheck :: Int -> IO [Integer]
 carmichaelCheck maxK = do
     first <- firstFail maxK carmichael
@@ -101,51 +109,32 @@ assignment5 = do
     print "You see that as you increasing k first fail does not decrease as fast as before"
     print "This is because carmichael numbers are special numbers: see code for details"
 
---6
+--6 - Miller-Rabin primality check
 {-
-    Use the list from the previous exercise to test the 
-    Miller-Rabin primality check. What do you find?
+    Uses miller rabin method to check if a non-prime is identified as a prime.
+    Returns the first non-prime were it went wrong
 
-    find: n-1 = 2^k * m
-
-    2) a^m `mod` n
-
-    3) p = (1 , k - 1)
+    Checks k times
+    (x:xs) takes a list of non-primes to check from
 -}
-
--- myFunc n m p k = if b == 1 then False else 
---                 if b == -1 then True else 
---                     if p == k - 1 then False else myFunc (p + 1) b m k
---                 where b = n^2 `mod` m
-
--- miller :: Integer -> IO Bool
--- miller n = do
---     a <- randomRIO (1, n-1) :: IO Integer
---     let k = gcd (n - 1) a
---     let (m, x) = quotRem (n - 1) (a ^ k)
---     print (show n ++ " " ++ show a ++ " " ++ show m ++ " " ++ show k)
---     let bo = a ^ m `mod` n
---     let prime = if bo == 1 then True else
---                     if bo == -1 then True else myFunc bo n 0 k
---     return $ prime
-
--- millerTest :: [Integer] -> IO Integer
--- millerTest (x:xs) = do
---     result <- miller x
---     final <- if result then millerTest xs else return x
---     return $ final
-
 firstFailMiler :: Int -> [Integer] -> IO Integer
+firstFailMiler k [] = error "No more non-primes to check from"
 firstFailMiler k (x:xs) = do
     isPrime <- primeMR k x
     next <- if isPrime then return x else firstFailMiler k xs
     return $ next
 
+{-
+    uses the miller rabin method with different k's to check were the first
+    non-prime is falsely identified as a prime
+
+    makK : maximum repetitions
+-}
 millerTest :: Int -> IO [Integer]
 millerTest maxK = do
     first <- firstFailMiler maxK carmichael
     next <- if (maxK > 1) then millerTest (maxK - 1) else return []
-    return (first : next)
+    return (next ++ [first])
 
 mersPrimes :: [Integer]
 mersPrimes = [2,3,5,7,13,17,19,31,61,89,107,127,521,607,1279,2203]
